5d9e49ec8146f6496e9995bbbfeaccfb5a919251
shawntez32 <shawntez.malone1@gmail.com>
2023-01-14 14:36:17 -0500

    Update

---
+++ src/ChannelRow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ChannelRow({image, channel, subs, numOfViews, description, verified}) {
+    return (
+        <>
+        <div className="channelRow">
+
+        </div>
+        </>
+    )
+}
+
+export default ChannelRow;
---
+++ src/VideoRow.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function VideoRow({views, subs, description, timestamp,channel,title,image}) {
+    return(
+        <>
+
+        </>
+    )
+}
