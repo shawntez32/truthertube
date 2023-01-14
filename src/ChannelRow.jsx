a4348939a0f7b618eed0b558173e93d594fb3350
shawntez32 <shawntez.malone1@gmail.com>
2023-01-14 14:58:02 -0500

    Update

--- src/ChannelRow.jsx
+++ src/ChannelRow.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { CheckCircleOutline } from "@material-ui/icons";
+import "./ChannelRow.css";
 
 function ChannelRow({image, channel, subs, numOfViews, description, verified}) {
     return (
         <>
         <div className="channelRow">
-
+            <img className="channelRow___logo" src={image} alt="" />
+            <div className="channelRow___text">
+                <h4>{channel} {verified && <CheckCircleOutline />}</h4>
+                <p>subscribers . {numOfViews} videos</p>
+                <p>{description}</p>
+            </div>
         </div>
         </>
     )
--- src/Searchpage.jsx
+++ src/Searchpage.jsx
@@ -2,6 +2,7 @@ import { TuneOutlined } from "@material-ui/icons";
 import React from "react";
 import Sidebar from "./Sidebar";
 import "./Searchpage.css";
+import ChannelRow from "./ChannelRow";
 
 function Searchpage() {
     return(
@@ -13,6 +14,15 @@ function Searchpage() {
                 <TuneOutlined />
                 <h2>Filter</h2>
             </div>
+
+            <ChannelRow
+            image="https://tse3.mm.bing.net/th?id=OIP.W47SKptrvaToph3irnXzVgHaFj&pid=Api&P=0"
+            channel="Idlehands4637"
+            subs="22,158"
+            numOfViews="8,007"
+            description="America will never be the same again after all of the turmoil currently going on"
+            verified
+            />
         </div>
         </div>
         </>
