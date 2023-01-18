d272efe83a0b5c3b9f98c5997f606523fd288798
shawntez32 <shawntez.malone1@gmail.com>
2023-01-18 17:29:25 -0500

    CSS Update

--- src/ReccomendedVideos.jsx
+++ src/ReccomendedVideos.jsx
@@ -10,65 +10,65 @@ function ReccomendedVideos() {
             <div className="reccomentions___videos">
             <VideoCard
             image="https://tse3.mm.bing.net/th?id=OIP.efyYSZuZzEtfbRu_k3KSPgHaEK&pid=Api&P=0"
-            title="Truth about wallstreet"
-            channel="NuancedThought"
-            views="1.6million"
-            timestamp="4 days ago"
+            title="Gokus Revenge"
+            channel="LucidAl"
+            views="320k"
+            timestamp="9 days ago"
             channelImg="https://tse3.mm.bing.net/th?id=OIP.UaCXcvpfxyLRZJNaP2mQgwHaE8&pid=Api&P=0"
              />
             <VideoCard
             image="https://tse2.mm.bing.net/th?id=OIP.Drmkmjw1uperFI7un8QN6wHaE7&pid=Api&P=0"
             title="Truth about wallstreet"
             channel="NuancedThought"
-            views="1.6million"
-            timestamp="4 days ago"
+            views="22k"
+            timestamp="2 weeks ago"
             channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXtjWPfUZP89i4nxsN5ozOqpULZOCrF4nvfQ&usqp=CAU"
-            title="Truth about wallstreet"
+            title="Does your vote really count?"
             channel="NuancedThought"
             views="1.6million"
-            timestamp="4 days ago"
+            timestamp="4 months ago"
             channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjDnXXy5vngPzr4t_PbtW-jNcvFhFsl6WT8w&usqp=CAU"
-            title="Truth about wallstreet"
+            title="Setting the stage"
             channel="NuancedThought"
-            views="1.6million"
-            timestamp="4 days ago"
+            views="48k"
+            timestamp="3 weeks ago"
             channelImg="https://tse3.mm.bing.net/th?id=OIP.UaCXcvpfxyLRZJNaP2mQgwHaE8&pid=Api&P=0"
              />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwI7I78ihvZADW1TVN8xrMinwaDSsHvcNiVtvmntyrz1pOVTsaxUD9UEy7kywR9A0sXXI&usqp=CAU"
-            title="Truth about wallstreet"
+            title="Origins of the illuminati"
             channel="NuancedThought"
-            views="1.6million"
-            timestamp="4 days ago"
+            views="89k"
+            timestamp="1 month ago"
             channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTEjkEnzPMXHf0TB_gvAwSR2W0R2cllA2u2dGEeEEGJOrHTgOJlvvxbM2CYJym_gZveIk&usqp=CAU"
-            title="Truth about wallstreet"
+            title="Plan for the NWO"
             channel="NuancedThought"
-            views="1.6million"
+            views="388k"
             timestamp="4 days ago"
             channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR49MdVmNmYMSG9wSATCksN_UrprWhVNlZdEg&usqp=CAU"
-            title="Truth about wallstreet"
+            title="Genius or Lunatic?"
             channel="NuancedThought"
-            views="1.6million"
+            views="5.2million"
             timestamp="4 days ago"
             channelImg="https://tse3.mm.bing.net/th?id=OIP.UaCXcvpfxyLRZJNaP2mQgwHaE8&pid=Api&P=0"
              />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsfgeRJD-7ROGVCg5TJlh2sf2_y_nRc0TCVg&usqp=CAU"
-            title="Truth about wallstreet"
+            title="Monopoly system"
             channel="NuancedThought"
-            views="1.6million"
+            views="215k"
             timestamp="4 days ago"
             channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
             />
@@ -82,7 +82,7 @@ function ReccomendedVideos() {
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhHoZLqc72QYs1gaXzGFhqaNSZCpkWD8Btvg&usqp=CAU"
-            title="Truth about wallstreet"
+            title="Follow the money"
             channel="NuancedThought"
             views="1.6million"
             timestamp="4 days ago"
@@ -90,7 +90,7 @@ function ReccomendedVideos() {
              />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB9nLcUvxBqaUaSrazlC_KPJba3TmSIHJwLQ&usqp=CAU"
-            title="Truth about wallstreet"
+            title="Corona Virus Conspiracy"
             channel="NuancedThought"
             views="1.6million"
             timestamp="4 days ago"
@@ -98,7 +98,7 @@ function ReccomendedVideos() {
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYgdJVyXzFVwbazkLPbXblYPnAAIKReGpLvg&usqp=CAU"
-            title="Truth about wallstreet"
+            title="Shape of the earth"
             channel="NuancedThought"
             views="1.6million"
             timestamp="4 days ago"
@@ -106,7 +106,7 @@ function ReccomendedVideos() {
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzLM5Y1t3sI1_IXlfIGjk5e6bma36gOGzllw&usqp=CAU"
-            title="Truth about wallstreet"
+            title="Are sports rigged"
             channel="NuancedThought"
             views="1.6million"
             timestamp="4 days ago"
@@ -122,7 +122,7 @@ function ReccomendedVideos() {
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAJmZlb1uMuAq4idi-51rimucCY-lXuH86EA&usqp=CAU"
-            title="Truth about wallstreet"
+            title="The effects of religion"
             channel="NuancedThought"
             views="1.6million"
             timestamp="4 days ago"
