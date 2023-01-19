de9f769bbb4c36f6cee136979e13abe45056ce82
shawntez32 <shawntez.malone1@gmail.com>
2023-01-19 15:49:43 -0500

    CSS Update

--- src/ReccomendedVideos.jsx
+++ src/ReccomendedVideos.jsx
@@ -14,20 +14,20 @@ function ReccomendedVideos() {
             channel="LucidAl"
             views="320k"
             timestamp="9 days ago"
-            channelImg="https://tse3.mm.bing.net/th?id=OIP.UaCXcvpfxyLRZJNaP2mQgwHaE8&pid=Api&P=0"
+            channelImg="https://tse2.explicit.bing.net/th?id=OIP.zY1k-FghfsCWoO3RYkdS6gAAAA&pid=Api&P=0"
              />
             <VideoCard
             image="https://tse2.mm.bing.net/th?id=OIP.Drmkmjw1uperFI7un8QN6wHaE7&pid=Api&P=0"
             title="Truth about wallstreet"
-            channel="NuancedThought"
+            channel="Amerigold"
             views="22k"
             timestamp="2 weeks ago"
-            channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
+            channelImg="https://tse2.explicit.bing.net/th?id=OIP.cmN7-lEwjAVPFu3iTxcZzQHaHZ&pid=Api&P=0"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXtjWPfUZP89i4nxsN5ozOqpULZOCrF4nvfQ&usqp=CAU"
             title="Does your vote really count?"
-            channel="NuancedThought"
+            channel="DarthBaker"
             views="1.6million"
             timestamp="4 months ago"
             channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
@@ -35,7 +35,7 @@ function ReccomendedVideos() {
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjDnXXy5vngPzr4t_PbtW-jNcvFhFsl6WT8w&usqp=CAU"
             title="Setting the stage"
-            channel="NuancedThought"
+            channel="St. trophy"
             views="48k"
             timestamp="3 weeks ago"
             channelImg="https://tse3.mm.bing.net/th?id=OIP.UaCXcvpfxyLRZJNaP2mQgwHaE8&pid=Api&P=0"
@@ -43,90 +43,90 @@ function ReccomendedVideos() {
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwI7I78ihvZADW1TVN8xrMinwaDSsHvcNiVtvmntyrz1pOVTsaxUD9UEy7kywR9A0sXXI&usqp=CAU"
             title="Origins of the illuminati"
-            channel="NuancedThought"
+            channel="Trx478"
             views="89k"
             timestamp="1 month ago"
-            channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
+            channelImg="https://tse1.mm.bing.net/th?id=OIP.6Wsq5MPp5VvwpXHKQ3Uc1QHaD7&pid=Api&P=0"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTEjkEnzPMXHf0TB_gvAwSR2W0R2cllA2u2dGEeEEGJOrHTgOJlvvxbM2CYJym_gZveIk&usqp=CAU"
             title="Plan for the NWO"
-            channel="NuancedThought"
+            channel="Blankman"
             views="388k"
             timestamp="4 days ago"
-            channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
+            channelImg="https://tse1.mm.bing.net/th?id=OIP.IAihLu8ftux-3P7AMbEj9QHaFj&pid=Api&P=0"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR49MdVmNmYMSG9wSATCksN_UrprWhVNlZdEg&usqp=CAU"
             title="Genius or Lunatic?"
-            channel="NuancedThought"
+            channel="ArmsRace"
             views="5.2million"
             timestamp="4 days ago"
-            channelImg="https://tse3.mm.bing.net/th?id=OIP.UaCXcvpfxyLRZJNaP2mQgwHaE8&pid=Api&P=0"
+            channelImg="https://tse1.mm.bing.net/th?id=OIP.lTV_rnusRcpB0a3RnSiK9AHaFp&pid=Api&P=0"
              />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsfgeRJD-7ROGVCg5TJlh2sf2_y_nRc0TCVg&usqp=CAU"
             title="Monopoly system"
-            channel="NuancedThought"
+            channel="Uncle Tito"
             views="215k"
             timestamp="4 days ago"
-            channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
+            channelImg="https://tse2.explicit.bing.net/th?id=OIP.Zp6lBY00uxucwsXXCSpE-wHaG4&pid=Api&P=0"
             />
             <VideoCard
-            image="https://tse3.mm.bing.net/th?id=OIP.efyYSZuZzEtfbRu_k3KSPgHaEK&pid=Api&P=0"
-            title="Truth about wallstreet"
-            channel="NuancedThought"
-            views="1.6million"
+            image="https://wallpapercave.com/wp/wp2665210.jpg"
+            title="Road to success"
+            channel="Lost Files"
+            views="72k"
             timestamp="4 days ago"
-            channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
+            channelImg="https://tse1.mm.bing.net/th?id=OIP.u5RiGZcjNPGx2pQmXu8bLQHaHa&pid=Api&P=0"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhHoZLqc72QYs1gaXzGFhqaNSZCpkWD8Btvg&usqp=CAU"
             title="Follow the money"
-            channel="NuancedThought"
-            views="1.6million"
+            channel="Amerigold"
+            views="2 months ago"
             timestamp="4 days ago"
-            channelImg="https://tse3.mm.bing.net/th?id=OIP.UaCXcvpfxyLRZJNaP2mQgwHaE8&pid=Api&P=0"
+            channelImg="https://tse2.mm.bing.net/th?id=OIP.uU5KysGxxIRx7vOS_k5ikgHaHa&pid=Api&P=0"
              />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB9nLcUvxBqaUaSrazlC_KPJba3TmSIHJwLQ&usqp=CAU"
             title="Corona Virus Conspiracy"
-            channel="NuancedThought"
-            views="1.6million"
-            timestamp="4 days ago"
-            channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
+            channel="Blox5467"
+            views="534k"
+            timestamp="1 year ago"
+            channelImg="https://tse3.mm.bing.net/th?id=OIP.W-7M1XrTgamvhE2GB2tOcQHaEK&pid=Api&P=0"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYgdJVyXzFVwbazkLPbXblYPnAAIKReGpLvg&usqp=CAU"
             title="Shape of the earth"
-            channel="NuancedThought"
+            channel="Artmop89d"
             views="1.6million"
             timestamp="4 days ago"
-            channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
+            channelImg="https://tse4.explicit.bing.net/th?id=OIP.qIa4wAMqfRZgU3QuiuguSQHaGS&pid=Api&P=0"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzLM5Y1t3sI1_IXlfIGjk5e6bma36gOGzllw&usqp=CAU"
             title="Are sports rigged"
-            channel="NuancedThought"
-            views="1.6million"
+            channel="Jsane"
+            views="88k"
             timestamp="4 days ago"
-            channelImg="https://tse3.mm.bing.net/th?id=OIP.UaCXcvpfxyLRZJNaP2mQgwHaE8&pid=Api&P=0"
+            channelImg="https://tse1.mm.bing.net/th?id=OIP.-F72Bzew4u_vOe_bHff-IQAAAA&pid=Api&P=0"
              />
             <VideoCard
-            image="https://tse2.mm.bing.net/th?id=OIP.Drmkmjw1uperFI7un8QN6wHaE7&pid=Api&P=0"
-            title="Truth about wallstreet"
-            channel="NuancedThought"
-            views="1.6million"
-            timestamp="4 days ago"
-            channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
+            image="https://tse3.mm.bing.net/th?id=OIP.Oc-T0TUXo2iuOBfQfLSbDAHaEo&pid=Api&P=0"
+            title="The nature of the soul"
+            channel="Bird's Eye"
+            views="32k"
+            timestamp="4 months ago"
+            channelImg="https://tse3.mm.bing.net/th?id=OIP.dxLGf8PRjY55o72jmSkGwQHaHq&pid=Api&P=0"
             />
             <VideoCard
             image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAJmZlb1uMuAq4idi-51rimucCY-lXuH86EA&usqp=CAU"
             title="The effects of religion"
-            channel="NuancedThought"
-            views="1.6million"
-            timestamp="4 days ago"
-            channelImg="https://avatars.githubusercontent.com/u/69469670?v=4"
+            channel="Reese Love"
+            views="16k"
+            timestamp="1 days ago"
+            channelImg="https://tse3.mm.bing.net/th?id=OIP.A-cxP3137pueNiRGwRCTZwAAAA&pid=Api&P=0"
             />
          </div>
         </div>
