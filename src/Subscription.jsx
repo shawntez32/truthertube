34d4b256dc5c3463a2455564e8150d0843c9ee45
shawntez32 <shawntez.malone1@gmail.com>
2023-01-21 09:55:02 -0500

    trending

--- src/Subscription.jsx
+++ src/Subscription.jsx
@@ -17,9 +17,27 @@ function Subscription({trending, subscriptions}) {
                 <Subscriptions />
                 <h2>Subscriptions</h2>
             </div>
-                <VideoRow />
-                <VideoRow />
-                <VideoRow />
+            <VideoRow
+                 image={"https://tse4.mm.bing.net/th?id=OIP.QdUYczu6fvdOQ94jXf8hEQHaEK&pid=Api&P=0"}
+                 title={"Nothing but politics"}
+                 channel={"Mr.Krank"}
+                 views={"657"}
+                 timestamp={"3 weeks ago"}
+                 />
+                <VideoRow
+                 image={"https://tse4.mm.bing.net/th?id=OIP.GQqEKlPCowzJXcVqVzpcrQHaEK&pid=Api&P=0"}
+                 title={"2 sides of the same coin."}
+                 channel={"Veloc"}
+                 views={"2k"}
+                 timestamp={"3 months ago"}
+                 />
+                <VideoRow
+                 image={"https://tse4.mm.bing.net/th?id=OIP.KQEdqZRwQyIpSY16dr9uoQHaE8&pid=Api&P=0"}
+                 title={"Showtime?!"}
+                 channel={"Mr.Krank"}
+                 views={"13k"}
+                 timestamp={"2 weeks ago"}
+                 />
         </div>
         </div>
         </>
