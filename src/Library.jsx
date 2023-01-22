9cfb64253676edfeab484660e9047ea7065f0e48
shawntez32 <shawntez.malone1@gmail.com>
2023-01-22 15:35:18 -0500

    Refactor

--- src/Library.jsx
+++ src/Library.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 
-function Library({subscriptions,trending}) {
+function Library({subscriptions,trending,library,history,yvids,lvids,wlater,smore}) {
     return(
         <>
         <div className="app___page">
             <Sidebar
             subscriptions={subscriptions}
             trending={trending}
+            library={library}
+            history={history}
+            yvids={yvids}
+            lvids={lvids}
+            wlater={wlater}
+            smore={smore}
             />
             <div className="noResults">
                 <h2>No Results</h2>
--- src/RickRoll.jsx
+++ src/RickRoll.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {DefaultPlayer as Video} from "react-html5video";
+import Sidebar from "./Sidebar";
 import "./Video.css";
 
 function RickRoll() {
@@ -9,6 +10,7 @@ function RickRoll() {
 
         <>
         <div className="app___page">
+            <Sidebar />
         <div className="video___frame">
         <iframe src="https://archive.org/embed/Rick_Astley_Never_Gonna_Give_You_Up&autoplay=1" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
         </div>
