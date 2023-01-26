b825b800275aa87d92a8d82c762d68abd8fdafdc
shawntez32 <shawntez.malone1@gmail.com>
2023-01-26 11:05:51 -0500

    Refactor

--- src/Comments.jsx
+++ src/Comments.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import VideoRow from "./VideoRow";
+import "./Video.css";
 
 function Comments() {
     return(
@@ -17,31 +19,58 @@ function Comments() {
                         Love this video!
                     </div>
                 </div>
+                <hr />
                 <div className="comment___default">
                     <div className="comment___avi">V</div>
                     <div className="comment___comment">
                         Got me again.
                     </div>
                 </div>
+                <hr />
                 <div className="comment___default">
                     <div className="comment___avi">R</div>
                     <div className="comment___comment">
                         This is so old.
                     </div>
                 </div>
+                <hr />
                 <div className="comment___default">
                     <div className="comment___avi">T</div>
                     <div className="comment___comment">
                         Rick rolled again? Thought I was done with this.
                     </div>
                 </div>
+                <hr />
                 <div className="comment___default">
                     <div className="comment___avi">P</div>
                     <div className="comment___comment">
                         Pls stop it
                     </div>
                 </div>
+                <hr />
             </div>
+            <hr className="vr_hr" />
+            <VideoRow
+                 image={"https://tse4.mm.bing.net/th?id=OIP.QdUYczu6fvdOQ94jXf8hEQHaEK&pid=Api&P=0"}
+                 title={"Nothing but politics"}
+                 channel={"Mr.Krank"}
+                 views={"657"}
+                 timestamp={"3 weeks ago"}
+                 />
+                <VideoRow
+                 image={"https://tse4.mm.bing.net/th?id=OIP.GQqEKlPCowzJXcVqVzpcrQHaEK&pid=Api&P=0"}
+                 title={"2 sides of the same coin."}
+                 channel={"Veloc"}
+                 views={"2k"}
+                 timestamp={"3 months ago"}
+                 />
+                <VideoRow
+                 image={"https://tse4.mm.bing.net/th?id=OIP.KQEdqZRwQyIpSY16dr9uoQHaE8&pid=Api&P=0"}
+                 title={"Showtime?!"}
+                 channel={"Mr.Krank"}
+                 views={"13k"}
+                 timestamp={"2 weeks ago"}
+                 />
             
         </div>
         </>
--- src/RickRoll.jsx
+++ src/RickRoll.jsx
@@ -22,7 +22,7 @@ function RickRoll({subscriptions,trending,library,history,yvids,lvids,wlater,smo
             smore={smore}
             />
         <div className="video___frame">
-        <iframe className="iframe" src="https://archive.org/embed/Rick_Astley_Never_Gonna_Give_You_Up&autoplay=1" width="640" height="480" frameborder="0" allow="autoplay" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
+        <iframe className="iframe" src="https://archive.org/embed/Rick_Astley_Never_Gonna_Give_You_Up&autoplay=1" width="1120" height="480" frameborder="0" allow="autoplay" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
         
         </div>
         </div>
